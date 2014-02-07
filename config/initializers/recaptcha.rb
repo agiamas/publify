7220388ab0581494ed5fcb47dfc08ee77637bfc7
alexandros giamas <agiamas@gmail.com>
2014-02-07 12:26:53 +0200

    recaptcha added

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = 'YourAPIkeysHere_yyyyyyyyyyyyyyyyy'
-  config.private_key = 'YourAPIkeysHere_xxxxxxxxxxxxxxxxx'
+  config.public_key  = '6LfgRe4SAAAAAMgSp-Y7kJ8MGjRB_1M3AyFiCn5e'
+  config.private_key = ENV['RECAPTCHA_PRIVATE_KEY']
 end
